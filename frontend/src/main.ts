46cc6459acd11fd304164478252af1d6ad0f8240
roguestone <roguestoneinweb@gmail.com>
2026-02-27 15:23:30 +0300

    chore: add primevue and Aura theme for now

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,6 +1,19 @@
+import Aura from '@primeuix/themes/aura';
+import PrimeVue from 'primevue/config';
 import { createApp } from 'vue';
 
 import './style.css';
 import App from './App.vue';
 
-createApp(App).mount('#app');
+createApp(App)
+  .use(PrimeVue, {
+    theme: {
+      options: {
+        cssLayer: false,
+        darkModeSelector: 'system',
+        prefix: 'p',
+      },
+      preset: Aura,
+    },
+  })
+  .mount('#app');
