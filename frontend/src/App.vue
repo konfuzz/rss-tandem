9370d0e8ba38953c2e43c945192631c4ba7d82d4
roguestone <roguestoneinweb@gmail.com>
2026-03-08 18:47:53 +0300

    feat: add widget config to temporarily render widgets

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,7 +1,26 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import type { WidgetConfig } from './types/widget';
+
+import WidgetWrapper from './components/WidgetWrapper.vue';
+
+const widgets: WidgetConfig[] = [
+  { type: 'example' },
+  {
+    type: 'basic-poll',
+  },
+  {
+    type: 'drag-n-drop',
+  },
+  {
+    type: 'ai-interviewer',
+  },
+];
+</script>
 
 <template>
-  <div></div>
+  <div>
+    <WidgetWrapper :widgets="widgets" />
+  </div>
 </template>
 
 <style scoped></style>
