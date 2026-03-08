af02d86903e78fe2581de53fe026621252af2fb1
roguestone <roguestoneinweb@gmail.com>
2026-03-08 18:49:31 +0300

    feat: add widget example to show how to render widgets, to be deleted later

---
+++ frontend/src/widgets/WidgetExample.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+import Card from 'primevue/card';
+</script>
+
+<template>
+  <Card>
+    <template #content>
+      <div class="text-center">
+        <p class="text-3xl font-bold">Example</p>
+      </div>
+    </template>
+  </Card>
+</template>
