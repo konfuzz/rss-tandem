19c82cb42d20c7584b2f8c3e0d62c80a17f1b0d5
roguestone <roguestoneinweb@gmail.com>
2026-03-11 22:22:52 +0300

    fix: add logic and comments in widget example

--- frontend/src/widgets/WidgetExample.vue
+++ frontend/src/widgets/WidgetExample.vue
@@ -1,5 +1,22 @@
 <script setup lang="ts">
 import Card from 'primevue/card';
+import { watch } from 'vue';
+
+const props = defineProps<{
+  validateTrigger: number;
+}>();
+
+const emit = defineEmits<{
+  validated: [boolean];
+}>();
+
+// функция валидации запускается при изменения пропса validateTrigger из родителя-обертки, когда происходит 1-й клик по кнопке "Ответить"
+function validate() {
+  const valid = true;
+  emit('validated', valid); // если valid = true, то кнопка в родителе меняется на "Следующий вопрос"
+}
+
+watch(() => props.validateTrigger, validate);
 </script>
 
 <template>
