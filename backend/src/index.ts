e0e20cccaac8373bd9b7203fd2cc1f2a49110c26
makesimple <80259445+konfuzz@users.noreply.github.com>
2026-03-19 11:55:41 +0400

    chore: initialize backend project (#28)

---
+++ backend/src/index.ts
@@ -0,0 +1,17 @@
+import cors from 'cors';
+import 'dotenv/config';
+import express from 'express';
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (_req, res) => {
+  res.json({ message: 'Backend is running' });
+});
+
+app.listen(port, () => {
+  console.log(`Server is running at http://localhost:${port}`);
+});
