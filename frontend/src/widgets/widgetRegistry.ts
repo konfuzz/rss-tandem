6b09f0a792ea3ebbe7b0ea9bdfed09bc9755ed72
konfuzz <konfuzz@gmail.com>
2026-03-25 11:47:35 +0400

    fix: fix linting error

--- frontend/src/widgets/widgetRegistry.ts
+++ frontend/src/widgets/widgetRegistry.ts
@@ -5,7 +5,7 @@ import { defineAsyncComponent } from 'vue';
 import type { questionType } from '../types/widget';
 
 export const widgetRegistry: Partial<Record<questionType, Component>> = {
-  'drag-n-drop': defineAsyncComponent(() => import('./MethodBuilder/MethodBuilderWidget.vue')),
   'ai-interviewer': defineAsyncComponent(() => import('./ai-interviewer/Ai-interviewer.vue')),
+  'drag-n-drop': defineAsyncComponent(() => import('./MethodBuilder/MethodBuilderWidget.vue')),
   example: defineAsyncComponent(() => import('./WidgetExample.vue')),
 };
